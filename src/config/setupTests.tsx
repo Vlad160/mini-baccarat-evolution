214b5bfdd710b66ff1d3aa2bf6d26ed4b7dfa062
Uladzislau Nestser <vlaner160@gmail.com>
2022-06-13 01:08:50 +0200

    remove ts-ignore

--- src/config/setupTests.tsx
+++ src/config/setupTests.tsx
@@ -1,4 +1,2 @@
 import nodeCrypto from 'crypto';
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-expect-error
-window.crypto = nodeCrypto;
+window.crypto = nodeCrypto as unknown as Crypto;
